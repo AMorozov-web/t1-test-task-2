5a39974dbdd2864ee4daf64fb93d66504929e1d4
Alex Morozov <a.morozov.work@mail.ru>
2021-07-18 22:47:36 +0300

    feat: add user-buttons-list component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import {MainSearch} from '../main-search/main-search';
 import {T1Logo} from '../t1-logo/t1-logo';
 import {TaxLogo} from '../tax-logo/tax-logo';
 import {UserBlock} from '../user-block/user-block';
+import {UserButtonsList} from '../user-buttons-list/user-buttons-list';
 
 const App = () => {
 
@@ -13,6 +14,7 @@ const App = () => {
       <T1Logo />
       <MainSearch />
       <UserBlock>
+        <UserButtonsList />
       </UserBlock>
     </Header>
   );
---
+++ src/components/user-buttons-list/user-buttons-list.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const unreadNotifyCount = 3;
+
+const List = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const Button = styled.button`
+  position: relative;
+  width: 40px;
+  height: 40px;
+  padding: none;
+  background-color: #ffffff;
+  background-position: center center;
+  background-size: 18px 18px;
+  background-repeat: no-repeat;
+  border: none;
+  cursor: pointer;
+
+  &::after {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: block;
+    width: 10px;
+    height: 10px;
+    font-size: 9px;
+    color: #ffffff;
+    background-color: #8f242f;
+    border-radius: 50%;
+  }
+`;
+
+const Notifications = styled(Button)`
+  background-image: url('img/notify.svg');
+
+  &::after {
+    content: '${unreadNotifyCount}';
+  }
+`;
+
+const Favorites = styled(Button)`
+  background-image: url('img/favorites.svg');
+`;
+
+const Options = styled(Button)`
+  background-image: url('img/options.svg');
+`;
+
+const Support = styled(Button)`
+  background-image: url('img/support.svg');
+`;
+
+const UserButtonsList = () => {
+
+  return (
+    <List>
+      <li>
+        <Notifications aria-label='Посмотреть уведомления'/>
+      </li>
+      <li>
+        <Favorites aria-label='Избранное'/>
+      </li>
+      <li>
+        <Options aria-label='Опции'/>
+      </li>
+      <li>
+        <Support aria-label='Техподдержка'/>
+      </li>
+    </List>
+  );
+};
+
+export {UserButtonsList};
