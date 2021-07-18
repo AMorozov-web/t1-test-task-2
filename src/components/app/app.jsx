2e8090ab079254fe79380cf8f7db8edacb19fd02
Alex Morozov <a.morozov.work@mail.ru>
2021-07-18 17:45:59 +0300

    feat: add main search component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from '../header/header';
+import {MainSearch} from '../main-search/main-search';
 import {T1Logo} from '../t1-logo/t1-logo';
 import {TaxLogo} from '../tax-logo/tax-logo';
 
@@ -9,6 +10,7 @@ const App = () => {
     <Header>
       <TaxLogo />
       <T1Logo />
+      <MainSearch />
     </Header>
   );
 };
