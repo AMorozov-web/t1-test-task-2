dff0ce9dd8e2eb1eefb1d068ee70fab8bbd3bfea
Alex Morozov <a.morozov.work@mail.ru>
2021-07-21 20:19:48 +0300

    style: style main component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledMain = styled.main`
+const MainContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, fit-content);
   grid-template-rows: repeat(4, fit-content);
+  grid-template-areas:  "nav title downloadButton",
+                        "nav declaration sum",
+                        "nav type type",
+                        "nav table table";
+  width: 990px;
+  margin: 0 auto;
 `;
 
 const Main = ({children}) => {
 
   return (
-    <StyledMain>
-      {children}
-    </StyledMain>
+    <main>
+      <MainContainer>
+        {children}
+      </MainContainer>
+    </main>
   );
 };
 
