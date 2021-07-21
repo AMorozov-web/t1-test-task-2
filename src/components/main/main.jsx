eeb9e2a7d764abe9f085afab6dbc207056833bc6
Alex Morozov <a.morozov.work@mail.ru>
2021-07-21 21:49:16 +0300

    fix: fix main styles

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 
 const MainContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, fit-content);
-  grid-template-rows: repeat(4, fit-content);
-  grid-template-areas:  "nav title downloadButton",
-                        "nav declaration sum",
-                        "nav type type",
+  grid-template-columns: repeat(3, min-content);
+  grid-template-rows: repeat(4, min-content);
+  grid-template-areas:  "nav title downloadButton"
+                        "nav declaration sum"
+                        "nav type type"
                         "nav table table";
   width: 990px;
   margin: 0 auto;
