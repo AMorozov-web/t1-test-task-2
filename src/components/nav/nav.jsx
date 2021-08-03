48911ba83f6f4096b93eb26f0bbd0d7496dcb5f5
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-08-03 08:00:52 +0300

    style: add currentColor to svg

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -25,6 +25,7 @@ const Link = styled.a`
   min-height: 23px;
   padding-left: 10px;
   padding-right: 27px;
+  color: ${Color.TUNDORA};
   text-decoration: none;
 
   &:hover ${StyledText},
@@ -36,22 +37,19 @@ const Link = styled.a`
     width: 18px;
     height: 17px;
     margin-right: 10px;
-    fill: ${Color.TUNDORA};
   }
 
-  &:hover svg,
-  &:focus svg {
-    fill: ${Color.BISMARK};
+  &:hover,
+  &:focus {
+    color: ${Color.BISMARK};
   }
 `;
 
 const LinkBack = styled(Link)`
+  color: ${Color.SILVER_CHALICE};
+
   & ${StyledText} {
     color: ${Color.SILVER_CHALICE};
-  }
-
-  & svg {
-    fill: ${Color.SILVER_CHALICE};
   }
 `;
 
@@ -62,6 +60,7 @@ const Summary = styled.summary`
   min-height: 31px;
   padding-left: 10px;
   padding-right: 33px;
+  color: ${Color.TUNDORA};
   background-repeat: no-repeat;
   background-position: center right 15px;
   background-size: 12px 12px;
@@ -77,12 +76,11 @@ const Summary = styled.summary`
     width: 18px;
     height: 17px;
     margin-right: 11px;
-    fill: ${Color.TUNDORA};
   }
 
-  &:hover svg,
-  &:focus svg {
-    fill: ${Color.BISMARK};
+  &:hover,
+  &:focus {
+    color: ${Color.BISMARK};
   }
 `;
 
@@ -107,6 +105,7 @@ const DropdownLink = styled.details`
   &[open] ${Summary} {
     min-height: 47px;
     padding-left: 25px;
+    color: ${Color.BISMARK};
     background-image: url('img/up-arrow.svg');
     background-position: center right 26px;
     background-color: #ffffff;
@@ -115,10 +114,6 @@ const DropdownLink = styled.details`
 
     & ${StyledText} {
       color: ${Color.BISMARK};
-    }
-
-    & svg {
-      fill: ${Color.BISMARK};
     }
   }
 `;
@@ -170,7 +165,7 @@ const Nav = () => {
       <List>
         <Item>
           <LinkBack href="" aria-label="Вернуться назад">
-            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M15.12 15.53L25 5.66a1 1 0 011.41 1.41l-9.06 9.06 8.8 8.8a1 1 0 010 1.41 1 1 0 01-1.42 0l-9.61-9.61a.85.85 0 010-1.2z"/><path d="M5.54 15.53l9.88-9.87a1 1 0 111.41 1.41l-9.06 9.06 8.8 8.8a1 1 0 010 1.41 1 1 0 01-1.41 0l-9.62-9.61a.85.85 0 010-1.2z"/></svg>
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M15.12 15.53L25 5.66a1 1 0 011.41 1.41l-9.06 9.06 8.8 8.8a1 1 0 010 1.41 1 1 0 01-1.42 0l-9.61-9.61a.85.85 0 010-1.2z" fill="currentColor"/><path d="M5.54 15.53l9.88-9.87a1 1 0 111.41 1.41l-9.06 9.06 8.8 8.8a1 1 0 010 1.41 1 1 0 01-1.41 0l-9.62-9.61a.85.85 0 010-1.2z" fill="currentColor"/></svg>
             <StyledText>
               Меню
             </StyledText>
@@ -178,7 +173,7 @@ const Nav = () => {
         </Item>
         <Item>
           <Link href="" aria-label="Главная">
-            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M29.71 15.29l-3-3-10-10a1 1 0 00-1.42 0l-10 10-3 3a1 1 0 000 1.42 1 1 0 001.42 0L5 15.41V29a1 1 0 001 1h20a1 1 0 001-1V15.41l1.29 1.3a1 1 0 001.42 0 1 1 0 000-1.42zM25 28H7V13.41l9-9 9 9z"/></svg>
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M29.71 15.29l-3-3-10-10a1 1 0 00-1.42 0l-10 10-3 3a1 1 0 000 1.42 1 1 0 001.42 0L5 15.41V29a1 1 0 001 1h20a1 1 0 001-1V15.41l1.29 1.3a1 1 0 001.42 0 1 1 0 000-1.42zM25 28H7V13.41l9-9 9 9z" fill="currentColor"/></svg>
             <StyledText>
               Главная
             </StyledText>
@@ -187,7 +182,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="Налоговая отчетность">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 Налоговая отчетность
               </StyledText>
@@ -248,7 +243,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="Бухгалтерская отчетность">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 Бухгалтерская отчетность
               </StyledText>
@@ -265,7 +260,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="СВК">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 СВК
               </StyledText>
@@ -282,7 +277,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="КХД">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 КХД
               </StyledText>
@@ -299,7 +294,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="Мотивированное мнение">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 Мотивированное мнение
               </StyledText>
@@ -315,7 +310,7 @@ const Nav = () => {
         </Item>
         <Item>
           <Link href="" aria-label="Запросы">
-            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M29.71 15.29l-3-3-10-10a1 1 0 00-1.42 0l-10 10-3 3a1 1 0 000 1.42 1 1 0 001.42 0L5 15.41V29a1 1 0 001 1h20a1 1 0 001-1V15.41l1.29 1.3a1 1 0 001.42 0 1 1 0 000-1.42zM25 28H7V13.41l9-9 9 9z"/></svg>
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M29.71 15.29l-3-3-10-10a1 1 0 00-1.42 0l-10 10-3 3a1 1 0 000 1.42 1 1 0 001.42 0L5 15.41V29a1 1 0 001 1h20a1 1 0 001-1V15.41l1.29 1.3a1 1 0 001.42 0 1 1 0 000-1.42zM25 28H7V13.41l9-9 9 9z" fill="currentColor"/></svg>
             <StyledText>
               Запросы
             </StyledText>
@@ -323,7 +318,7 @@ const Nav = () => {
         </Item>
         <Item>
           <Link href="" aria-label="Статистика">
-            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M29.71 15.29l-3-3-10-10a1 1 0 00-1.42 0l-10 10-3 3a1 1 0 000 1.42 1 1 0 001.42 0L5 15.41V29a1 1 0 001 1h20a1 1 0 001-1V15.41l1.29 1.3a1 1 0 001.42 0 1 1 0 000-1.42zM25 28H7V13.41l9-9 9 9z"/></svg>
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M29.71 15.29l-3-3-10-10a1 1 0 00-1.42 0l-10 10-3 3a1 1 0 000 1.42 1 1 0 001.42 0L5 15.41V29a1 1 0 001 1h20a1 1 0 001-1V15.41l1.29 1.3a1 1 0 001.42 0 1 1 0 000-1.42zM25 28H7V13.41l9-9 9 9z" fill="currentColor"/></svg>
             <StyledText>
               Статистика
             </StyledText>
@@ -332,7 +327,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="Нормативные документы">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 Нормативные документы
               </StyledText>
@@ -349,7 +344,7 @@ const Nav = () => {
         <Item>
           <DropdownLink aria-label="О компании">
             <Summary>
-              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z"/><circle cx="4.72" cy="10.65" r=".81"/><circle cx="7.78" cy="10.65" r=".81"/><circle cx="10.84" cy="10.65" r=".81"/><circle cx="13.91" cy="10.65" r=".81"/><circle cx="4.72" cy="14.45" r=".82"/><circle cx="7.78" cy="14.45" r=".82"/><circle cx="10.84" cy="14.45" r=".82"/><circle cx="13.91" cy="14.45" r=".82"/><circle cx="4.72" cy="18.25" r=".82"/><circle cx="7.78" cy="18.25" r=".82"/><circle cx="10.84" cy="18.25" r=".82"/><circle cx="13.91" cy="18.25" r=".82"/></svg>
+              <svg viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.12 0H.5a.5.5 0 00-.5.5v22.75a.5.5 0 00.5.5h17.62a.5.5 0 00.5-.5V.5a.5.5 0 00-.5-.5zm-.5 22.75H1V1h16.62z" fill="currentColor"/><path d="M4.06 7.05h10.5a.5.5 0 00.5-.5V3.68a.5.5 0 00-.5-.5H4.06a.5.5 0 00-.5.5v2.87a.5.5 0 00.5.5zm.5-2.87h9.5v1.87h-9.5z" fill="currentColor"/><circle cx="4.72" cy="10.65" r=".81" fill="currentColor"/><circle cx="7.78" cy="10.65" r=".81" fill="currentColor"/><circle cx="10.84" cy="10.65" r=".81" fill="currentColor"/><circle cx="13.91" cy="10.65" r=".81" fill="currentColor"/><circle cx="4.72" cy="14.45" r=".82" fill="currentColor"/><circle cx="7.78" cy="14.45" r=".82" fill="currentColor"/><circle cx="10.84" cy="14.45" r=".82" fill="currentColor"/><circle cx="13.91" cy="14.45" r=".82" fill="currentColor"/><circle cx="4.72" cy="18.25" r=".82" fill="currentColor"/><circle cx="7.78" cy="18.25" r=".82" fill="currentColor"/><circle cx="10.84" cy="18.25" r=".82" fill="currentColor"/><circle cx="13.91" cy="18.25" r=".82" fill="currentColor"/></svg>
               <StyledText>
                 О компании
               </StyledText>
